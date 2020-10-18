40a534e4b36477d362aa86411695a19dc0c8b789
Andrew Plassard <andrew.plassard@gmail.com>
2020-10-18 13:43:52 -0400

    Updated functionality to get a game

--- tests/xxanalyzer/game/test_game.py
+++ tests/xxanalyzer/game/test_game.py
@@ -1,9 +1,21 @@
 import pytest
-import 18
+from xxanalyzer.game import Game
 
 @pytest.fixture
 def game_id_18ms():
     return "12886"
 
-def test_get_game(game_id_18ms):
+@pytest.fixture
+def game_18ms(game_id_18ms):
+    return Game(game_id_18ms)
+
+def test_game_id_18ms(game_id_18ms):
     assert game_id_18ms == "12886"
+
+def test_game_class(game_18ms):
+    assert game_18ms.game_id == "12886"
+
+def test_game_class_info(game_18ms):
+    assert game_18ms.game_info
+    assert game_18ms.game_info["id"] == 12886
+    assert len(game_18ms.game_info["players"]) == 3
--- xxanalyzer/game/game.py
+++ xxanalyzer/game/game.py
@@ -1,9 +1,11 @@
 """interfaces for an 18xx game"""
+import requests
 
 def download_game(game_id):
-    pass
+    url = f"https://www.18xx.games/api/game/{game_id}"
+    return requests.get(url).json()
 
 class Game:
     def __init__(self, game_id):
-        self.game_id
+        self.game_id = game_id
         self.game_info = download_game(self.game_id)
